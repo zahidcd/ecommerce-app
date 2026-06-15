a2607b3393bd5fee99f4cae325a9ecd8a8195020
Zahid Ceopa <zahidmartin@hotmail.com>
2026-06-14 22:29:00 -0500

    Implement Lombok

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -17,10 +17,6 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
-
     @GetMapping("/public/categories")
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> categories = categoryService.getAllCategories();
